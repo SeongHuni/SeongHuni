95ee186e1a86245b707608a9d183fcbe7ab7c03c
SeongHuni <118410211+SeongHuni@users.noreply.github.com>
2025-04-04 21:45:17 +0900

    Update Term.java

--- polynomal/Term.java
+++ polynomal/Term.java
@@ -1,24 +1,27 @@
-package polynomal;
+package Polynomial;
+
 
 public class Term {
-    private int constant; // 계수
-    private int exponent;    // 지수
+  private final int constant;
+  private final int exponent;
 
-    public Term(int coefficient, int exponent) {
-        this.constant = coefficient;
-        this.exponent = exponent;
-    }
+  public Term(int constant, int exponent) {
+    this.constant = constant;
+    this.exponent = exponent;
+  }
 
-    public int getConstant() {
-        return constant;
-    }
+  public void getConstant() {
+    return constant;
+  }
 
-    public int getExponent() {
-        return exponent;
-    }
+  public void getExponent() {
+    return exponent;
+  }
 
-    @Override
-    public String toString() {
-        return constant + (exponent != 0 ? "x^" + exponent : "");
-    }
+  @Override
+  public String toString() {
+    return constant + "x^" + exponent;
+  }
+
+
 }
